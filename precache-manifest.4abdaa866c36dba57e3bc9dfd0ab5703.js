edb3e5349c63429f9b769deb21b057fafa57e7bc
Cristian Orozco <cristianorozco1984@gmail.com>
2020-07-19 19:19:46 -0500

    Updates

---
+++ precache-manifest.4abdaa866c36dba57e3bc9dfd0ab5703.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac5aa7ea3317932de7982c38fde946e3",
+    "url": "/voice-recognition/index.html"
+  },
+  {
+    "revision": "406d15f1bea5d82b934f",
+    "url": "/voice-recognition/static/css/main.78c9f435.chunk.css"
+  },
+  {
+    "revision": "c79fbbdecbc6c9cfb6f2",
+    "url": "/voice-recognition/static/js/2.c2b9aa4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/voice-recognition/static/js/2.c2b9aa4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "406d15f1bea5d82b934f",
+    "url": "/voice-recognition/static/js/main.654cf5a4.chunk.js"
+  },
+  {
+    "revision": "f60b87d050e2f0eb4e68",
+    "url": "/voice-recognition/static/js/runtime-main.1542db8b.js"
+  }
+]);
